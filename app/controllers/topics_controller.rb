8c843ce2c88c76cae081e4b5362efdd23febb35b
bingo8670 <307286130@qq.com>
2018-08-07 17:50:44 +0800

    -1 vote and about

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -67,6 +67,15 @@ class TopicsController < ApplicationController
     redirect_to(topics_path)
   end
 
+  def downvote
+    @topic = Topic.find(params[:id])
+    @topic.voters.last.destroy if @topic.voters.count > 0
+    redirect_to(topics_path)
+  end
+  
+  def about
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_topic
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :topics do
     member do
       post 'upvote'
+      post 'downvote'
     end
   end
   root 'topics#index'
+  get 'about' => "topics#about"
 end
